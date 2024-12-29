7d73591df15693d2f24fc1bdd2f25b2f696a89ba
memberclassbr <micaelsgarcez@gmail.com>
2024-12-29 18:58:11 -0300

    Fix: build bugs

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    ppr: true,
-  },
   images: {
     remotePatterns: [
       {
-        hostname: 'avatar.vercel.sh',
-      },
-    ],
-  },
-};
+        hostname: 'avatar.vercel.sh'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
